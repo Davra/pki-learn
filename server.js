59b2f8dc00ca0b3d2e994a259386000e2263b42e
Colin <colin.ryan@davra.com>
2025-04-24 22:22:50 +0100

    initial dump of artifacts

---
+++ server.js
@@ -0,0 +1,45 @@
+const { createServer } = require('node:tls');
+const { readFileSync } = require('node:fs');
+
+const options = {
+  key: readFileSync('/home/colin/workspace/pki-example-1/certs/simple-org.key'),
+  cert: readFileSync('/home/colin/workspace/pki-example-1/fullchain.pem'),
+
+  // This is necessary only if using client certificate authentication.
+  requestCert: true,
+
+  // This is necessary only if the client uses a self-signed certificate.
+  ca: [ readFileSync('/home/colin/workspace/pki-example-1/ca_chain.pem') ],
+
+  crl: [
+    readFileSync('/home/colin/workspace/pki-example-1/crl/signing-ca.crl'),
+    readFileSync('/home/colin/workspace/pki-example-1/crl/root-ca.crl'),
+  ]
+};
+
+const server = createServer(options, (socket) => {
+  console.log('server connected',
+              socket.authorized ? 'authorized' : 'unauthorized');
+  socket.write('welcome!\n');
+  socket.setEncoding('utf8');
+  socket.pipe(socket);
+});
+server.listen(5555, () => {
+  console.log('server bound');
+});
+
+server.on('tlsClientError', (err, tlsSocket) => {
+    console.log('Client error, socket status: ',
+        tlsSocket.authorized ? 'authorized' : 'unauthorized');
+    console.error('cert error', tlsSocket.authorizationError);
+    console.error('Error from client:', err);
+});
+
+server.on('keylog', (line, tlsSocket) => {
+    
+    //console.log(line);
+  }); 
+
+server.on('error', err => {
+    console.error('Server error', err);
+});
